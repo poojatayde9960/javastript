f4f70e161c2a32b1e564505532d222d0b3465851
pooja <https://github.com/poojatayde9960/ecom.git>
2025-01-19 13:00:44 +0530

    init repo

---
+++ variable.js
@@ -0,0 +1,38 @@
+// Variables - To assign value
+
+var x
+var passion;    //declaration
+
+x = 50
+passion = 'Gaming';  //intializion
+
+const z = 55
+var rating = 7.5    //declaration and intailizion
+
+demo = 500
+var firstNames = 'Pentacost';   //auto declaare
+
+console.log(passion, rating, firstNames)
+
+// Const - Cannot changes the value or re assign
+const Name = 'Ironman'
+
+console.log(Name,)
+
+// Let - To change perticular value as per need when we wanted 
+{
+    let passion = 'Photgraphy';
+    console.log(passion);
+}
+console.log(passion);
+
+// Way to declare variable
+
+const firstName = "Akshay"
+const first_name = "Akshay"
+// const 1fn = "Akshay"    //error
+const _1fn = "Akshay"
+const $1fn = "Akshay"
+//  const first&lastName = "Akshay"   //error
+
+console.log(firstName);
